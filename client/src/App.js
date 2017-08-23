d2e04f04772a81b20a3656f2454e4cb80f924328
MdL <mdlmdel@gmail.com>
2017-08-23 18:43:33 -0400

    Add project

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { DB_CONFIG } from './config/config'; 
+import firebase from 'firebase/app';
+import 'firebase/database';
+
 import './App.css';
+import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm';
+import './Note/Note.css';
+import './NoteForm/NoteForm.css';
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.addNote = this.addNote.bind(this);
+    this.removeNote = this.removeNote.bind(this);
+
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('notes');
+
+    // We're going to setup the React state of our component
+    this.state = {
+      notes: [],
+    }
+  }
+
+  componentWillMount(){
+    const previousNotes = this.state.notes;
+
+    // DataSnapshot
+    this.database.on('child_added', snap => {
+      previousNotes.push({
+        id: snap.key,
+        noteContent: snap.val().noteContent,
+      })
+
+      this.setState({
+        notes: previousNotes
+      })
+    })
+
+    this.database.on('child_removed', snap => {
+      for(var i=0; i < previousNotes.length; i++){
+        if(previousNotes[i].id === snap.key){
+          previousNotes.splice(i, 1);
+        }
+      }
+
+      this.setState({
+        notes: previousNotes
+      })
+    })
+  }
+
+  addNote(note){
+    this.database.push().set({ noteContent: note});
+  }
+
+  removeNote(noteId){
+    console.log("from the parent: " + noteId);
+    this.database.child(noteId).remove();
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">React & Firebase To-Do List</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="notesBody">
+          {
+            this.state.notes.map((note) => {
+              return (
+                <Note noteContent={note.noteContent} 
+                noteId={note.id} 
+                key={note.id} 
+                removeNote={this.removeNote}/>
+              )
+            })
+          }
+        </div>
+        <div className="notesFooter">
+          <NoteForm addNote={this.addNote} />
+        </div>
       </div>
     );
   }
