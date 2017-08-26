fe06154fad2cc0a584297ba8e46e65d4fd83016f
MdL <mdlmdel@gmail.com>
2017-08-26 08:08:39 -0400

    Update project

--- client/src/config/config.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-    apiKey: "AIzaSyBNsoU9SMGRSeY4yt6lb1qqPo07d38hmoQ",
-    authDomain: "todo-react-firebase-e046b.firebaseapp.com",
-    databaseURL: "https://todo-react-firebase-e046b.firebaseio.com",
-    projectId: "todo-react-firebase-e046b",
-    storageBucket: "todo-react-firebase-e046b.appspot.com",
-    messagingSenderId: "1975522068"
-  };
